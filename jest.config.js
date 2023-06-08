ea2af0fcc7a01d36222fef660c89983d2584139a
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-06-09 01:52:55 +0700

    test: setup Jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        diagnostics: false,
+      },
+    ],
+  },
+
+  verbose: true,
+};
