33ab627a0d5714462a36555d7d8a818773d8db3a
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-06-08 16:55:01 +0700

    feat: add parser
    Add a class for parser.

---
+++ src/lib/parser.ts
@@ -0,0 +1,21 @@
+import Command from "./commands/command";
+import CommandFactory from "./commands/commandFactory";
+import Result from "./result";
+
+export default class Parser {
+  #commandMapping: Map<string, CommandFactory>;
+
+  constructor(commandMapping: Map<string, CommandFactory>) {
+    this.#commandMapping = commandMapping;
+  }
+
+  parse(rawString: string): Result<Command> {
+    const commandName = rawString.trimStart().split(" ")[0];
+    const commandFactory = this.#commandMapping.get(commandName);
+
+    if (commandFactory === undefined) {
+      return Result.err("ERR unknown command");
+    }
+    return Result.ok(commandFactory.create(rawString));
+  }
+}
