9a8077db4561cd5cc3620a6a814a08431c94c671
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-06-09 16:03:06 +0700

    feat: expose StoreMediator in the api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,6 @@ import Parser from "./parser";
 import Store from "./store";
 import Result from "./result";
 import Logger from "./logger";
+import StoreMediator from "./storeMediator";
 
-export { Parser, Store, Result, Logger };
+export { Parser, Store, Result, Logger, StoreMediator };
