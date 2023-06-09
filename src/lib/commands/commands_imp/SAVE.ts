3f8f49815cdeca3ccb558bad09b2d82a7cb44632
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-06-09 23:23:28 +0700

    fix!: make SAVE execute() return a string
    This is for more clarity than returning a simple boolean.

--- src/lib/commands/commands_imp/SAVE.ts
+++ src/lib/commands/commands_imp/SAVE.ts
@@ -3,9 +3,9 @@ import Result from "../../result";
 import Store from "../../store";
 import StoreMediator from "../../storeMediator";
 export default class SAVECommand extends Command {
-  execute(mediator: StoreMediator): Result<number> {
+  execute(mediator: StoreMediator): Result<string> {
     mediator.takeSnapshot();
-    return Result.ok(1);
+    return Result.ok("OK");
   }
   toString(): string {
     return `SAVE`;
